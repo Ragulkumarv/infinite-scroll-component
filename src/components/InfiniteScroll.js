8bc7b5840e10c94a25ee5658cb3072f45338845d
Ragul <ragulkumarv333@gmail.com>
2025-03-16 17:57:04 +0530

    Update InfiniteScroll.js

--- src/components/InfiniteScroll.js
+++ src/components/InfiniteScroll.js
@@ -7,6 +7,8 @@ const options = {
   threshold: 1.0,
 };
 
+const fallbackImg = `https://placehold.co/300x450/cccccc/ffffff?text=No+Image`;
+
 const InfiniteScroll = () => {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
@@ -54,17 +56,21 @@ const InfiniteScroll = () => {
   }, [hasMore]);
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {posts.map((item) => (
           <section
             key={item.id}
             className="bg-white rounded-lg shadow-md overflow-hidden"
           >
             <img
-              src={TMDB_IMG_Path + item.poster_path}
+              src={
+                item.poster_path
+                  ? TMDB_IMG_Path + item.poster_path
+                  : fallbackImg
+              }
               alt={`${item.original_title} Poster`}
-              className="w-full h-96 sm:h-60 object-contain sm:object-fill transform transition-transform duration-300 hover:scale-105 hover:cursor-pointer"
+              className="w-full h-96 sm:h-60 object-fill sm:object-contain transform transition-transform duration-300 hover:scale-105 hover:cursor-pointer"
               loading="lazy"
             />
             <div className="p-4">
